1014886b3485f8edba67ff84976ed10f36aac669
serignembackecoly <smcoly1@gmail.com>
2024-10-11 23:07:52 +0000

    add the french version

--- code/triangle_chart.R
+++ code/triangle_chart.R
@@ -4,7 +4,7 @@ library(fmsb)
 library(Cairo)
 
 # The triangle chart
-lvi_ipcc <- read.csv2("LVI_IPCC.csv", header = T, check.names = F)
+lvi_ipcc <- read.csv2("data/LVI_IPCC.csv", header = T, check.names = F)
 
 Max <- data.frame(rbind(rep(0.7,3)))
 Min <- data.frame(rbind(rep(0,3)))
@@ -14,9 +14,9 @@ min_max <- min_max %>%
 colnames(min_max) <- colnames(lvi_ipcc)
 data_ipcc <- rbind(min_max, lvi_ipcc) %>% 
               column_to_rownames(var = "rowname")
-varnames <- c("Adaptive\nCapacity", "Sensitivity", "Exposure" )
+varnames <- c("Capacité \n d'Adaptation", "Sensibilité", "Exposition" )
 #png("SpiderDiagramLVI-IPCC.png", res = 500, height = 14, width = 15, units = "cm")
-Cairo("Spider_diagram_LVI-IPCC.png", res = 500,
+Cairo::Cairo("Spider_diagram_LVI-IPCC_french.png", res = 500,
       height = 15, width = 15,bg = "white", units = "cm") 
 
 disposition <- par(mar = c(1, 2, 2, 2))
@@ -40,48 +40,4 @@ legend(
   text.col = "black", cex = 1, pt.cex = 1
 )
 par(disposition)
-dev.off()
-
-
-
-# Load necessary libraries
-library(tidyverse)
-library(fmsb)
-library(Cairo)
-
-# Load data
-lvi_ipcc <- read_csv2("data/LVI_IPCC.csv")
-
-# Create maximum and minimum values
-max_values <- data.frame(rbind(rep(0.7, 3)))
-min_values <- data.frame(rbind(rep(0, 3)))
-min_max <- rbind(max_values, min_values)
-min_max <- min_max %>%
-  mutate(group = c("Max", "Min"), .before = 1)
-
-# Combine data and create radar chart
-colnames(min_max) <- colnames(lvi_ipcc)
-data_ipcc <- rbind(min_max, lvi_ipcc)
-rownames(data_ipcc) <- data_ipcc$rowname
-data_ipcc <- data_ipcc[, -1]  # Remove the "rowname" column
-
-# Radar chart parameters
-var_names <- c("Adaptive\nCapacity", "Sensitivity", "Exposure")
-
-# Create radar chart
-Cairo("figure/spider_diagram_LVI-IPCC.png", res = 500, height = 15, width = 15, bg = "white", units = "cm")
-par(mar = c(1, 2, 2, 2))
-radarchart(data_ipcc, axistype = 1,
-           cglcol = "gray55", cglty = 1, axislabcol = "black", caxislabels = seq(0, 0.7, 0.1), cglwd = 0.8,
-           vlcex = 1.3, plwd = 1.5, seg = 7,
-           pcol = c("red", "blue", "green"),
-           vlabels = var_names)
-
-# Add legend
-legend(
-  x = "topright", legend = c("Kongoussi", "Léo", "Gaoua"),
-  bty = "n", pch = 16, col = c("red", "blue", "green"),
-  text.col = "black", cex = 1, pt.cex = 1
-)
-par(op)
 dev.off()
